4d37e62d373d2abb7ae539df28e737fc42b6b68c
Karladelangel8 <karladelangelsantiago03@gmail.com>
2024-05-10 17:19:06 -0600

    anexe cambios

---
+++ my-react-app/src/Components/Main.jsx
@@ -0,0 +1,14 @@
+import './Main.css';
+
+
+const Main = () => (
+    <>
+        <h1>Karla Del Angel</h1>
+        <h2>Me presento</h2>
+        <h2>Soy matemática aplicada a la computación, con experiencia en la resolución de problemas complejos  mediante algoritmos y modelado matemático. Disfruto explorar y dominar diferentes lenguajes de programación</h2>
+        <p><small>karlavanessadelangel@gmail.com | IG: vane_cliket</small></p>
+    </>
+)
+
+
+export default Main;
